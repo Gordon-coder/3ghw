60df1c0fe59ccadc6f6a4de0f9877bac0278ab58
gordon <gordonpeng27@gmail.com>
2024-09-11 22:32:36 +0800

    initial commit

--- api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Assignment
-
-class WorkSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Assignment
-        fields = '__all__'
